075e189068f8858211d7f061c85d0f8b501711c6
S&L <sl.beauty1999@gmail.com>
2025-10-24 02:39:55 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 function App() {
